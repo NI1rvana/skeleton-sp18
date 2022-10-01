a0ff9b6bfb5ec07428b9d74123b6a4528777f4ab
NI1rvana <92633189+NI1rvana@users.noreply.github.com>
2022-10-01 20:38:44 +0800

    Update LinkedListDeque.java

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -4,44 +4,63 @@ public class LinkedListDeque<Node> {
     private class Intnode {
         public Node item;
         public Intnode next;
-        public Intnode(Node x, Intnode n) {
+        public Intnode prev;
+        public Intnode(Node x) {
             item = x;
-            next = n;
+            next = null;
+            prev = null;
         }
     }
     public Intnode head,tail;
     public int size;
     public LinkedListDeque() {
-        this.head = new Intnode(null ,null);
-        this.tail = head;
+        this.head = new Intnode(null);
+        this.tail = new Intnode(null);
         this.size = 0;
     }
 
     public void addFirst(Node x) {
-        if(this.isEmpty())
-        {
-            head.next = new Intnode(x,head.next);
-            tail = head.next;
+        if(this.isEmpty()) {
+            Intnode p = new Intnode(x);
+            head.next = p;
+            tail.prev = p;
         }
-        else
-            this.head.next = new Intnode(x, head.next);
+        else {
+            Intnode p = new Intnode(null);
+            head.item = x;
+            head.prev = p;
+            p.next = head;
+            head = p;
+        }
         this.size += 1;
     }
     public void addLast(Node x) {
         if(this.isEmpty()) {
-            head.next = new Intnode(x,head.next);
-            tail = head.next;
+            this.addFirst(x);
         }
         else {
-            this.tail.next = new Intnode(x, null);
-            this.tail = this.tail.next;
+            Intnode p = new Intnode(null);
+            tail.item = x;
+            tail.next = p;
+            p.prev = tail;
+            tail = p;
         }
         this.size += 1;
     }
 
-    public void removeFirst() {
+    public Node removeFirst() {
+        Node p = head.next.item;
         this.head = head.next;
+        this.head.prev = null;
         this.size -= 1;
+        return p;
+    }
+    public Node removeLast() {
+        Node p = tail.prev.item;
+        this.tail = tail.prev;
+        this.tail.next = null;
+        this.size -= 1;
+        return p;
     }
     private Node getRecursiveHelp(Intnode start, int index) {
         if (index == 0) {
@@ -66,8 +85,12 @@ public class LinkedListDeque<Node> {
     }
 
     public boolean isEmpty() {
-        if (head == tail) return true;
-        else return false;
+        if (head == tail) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     public int size() {
         return this.size;
