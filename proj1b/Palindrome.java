6317df8fd4005481a887b6c703432057f04bff57
NI1rvana <92633189+NI1rvana@users.noreply.github.com>
2022-10-07 18:02:21 +0800

    Update Palindrome.java

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,32 +1,31 @@
-
 public class Palindrome {
+    /** convert a word to dq. */
     public Deque<Character> wordToDeque(String word) {
-        Deque<Character> w = new ArrayDeque<>();
-        for (int i = 0; i < word.length(); i++) {
-            w.addLast(word.charAt(i));
+        Deque<Character> dq = new ArrayDeque<>();
+        int len = word.length();
+        for (int i = 0; i < len; i++) {
+            dq.addLast(word.charAt(i));
         }
-        return w;
+        return dq;
     }
 
+    /** decide if the given word is palindrome. */
     public boolean isPalindrome(String word) {
-        Deque<Character> w = this.wordToDeque(word);
-        int flag = 1;
-        int size = w.size();
-        if (size == 1 || size == 0) {
+        if (word == null || word.length() <= 1) {
             return true;
         }
-        while (w.size() > 1) {
-            if (w.removeFirst() != w.removeLast()) {
-                flag = 0;
-                break;
+        int len = word.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (word.charAt(i) != word.charAt(len - i - 1)) {
+                return false;
             }
         }
-        if (flag == 0) {
-            return false;
-        }
-        return  true;
+        return true;
     }
 
+    /** overloaded isPalindrome, decide if the given word is palindrome.
+     * according to the given CharacterComparator
+     */
     public boolean isPalindrome(String word, CharacterComparator cc) {
         if (word == null || word.length() <= 1) {
             return true;
