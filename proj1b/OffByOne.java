714ed45316ead4c6cb39cd18defc1e30b041b15f
NI1rvana <92633189+NI1rvana@users.noreply.github.com>
2022-10-07 23:37:28 +0800

    Update OffByOne.java

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,6 +1,8 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        return Math.abs(x - y) == 1;
+        char a = Character.toLowerCase(x);
+        char b = Character.toLowerCase(y);
+        return Math.abs(a - b) == 1;
     }
 }
